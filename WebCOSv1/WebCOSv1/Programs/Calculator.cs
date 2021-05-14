0646f43f0930d1c39b7713d18aa6f539b46caefa
Izaak Kuipers <76709090+TechWorldInc@users.noreply.github.com>
2021-05-14 22:00:42 +0200

    Update Calculator.cs

--- WebCOSv1/WebCOSv1/Programs/Calculator.cs
+++ WebCOSv1/WebCOSv1/Programs/Calculator.cs
@@ -1,21 +1,24 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Text;
 using Cosmos.HAL;
+
 namespace TWIx86.Programs
 {
     class Calculator
     {
+        public static string displayText = "";
+
+        public static char[] validChars = {
+            '1','2','3','4','5','6','7','8','9','0','.','+','-','*','/'
+        };
+
         public static void Draw()
         {
-            Core.Running = false;
-            Console.BackgroundColor = ConsoleColor.Green;
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.Clear();
-            //Start drawing//
-            setToolbarStatus("Drawing... Please wait...", ConsoleColor.Cyan, ConsoleColor.White);
+            userInterface.drawRectangle(30, 7, 22, 13, ConsoleColor.Black);
             userInterface.drawRectangle(29, 6, 22, 13, ConsoleColor.White);
-            userInterface.drawRectangle(31, 7, 18, 3, ConsoleColor.Blue);
+            userInterface.drawRectangle(31, 7, 18, 3, ConsoleColor.DarkCyan);
             userInterface.drawRectangle(31, 11, 3, 1, ConsoleColor.Blue);
             userInterface.drawRectangle(36, 11, 3, 1, ConsoleColor.Blue);
             userInterface.drawRectangle(41, 11, 3, 1, ConsoleColor.Blue);
@@ -48,12 +51,50 @@ namespace TWIx86.Programs
             userInterface.WriteString(37, 17, ".", ConsoleColor.Yellow, ConsoleColor.Blue);
             userInterface.WriteString(42, 17, "-", ConsoleColor.Yellow, ConsoleColor.Blue);
             userInterface.WriteString(47, 17, "=", ConsoleColor.Yellow, ConsoleColor.Blue);
-            //Stop drawing///
+        }
+
+        public static void checkKey()
+        {
+
+            var key = Console.ReadKey(true);
+            Draw();
             setToolbarStatus("TWIx86 Calculator", ConsoleColor.Cyan, ConsoleColor.White);
-            Console.SetCursorPosition(0, 0);
-            Console.ReadKey();
-            stopCalculator();
-            
+            if (key.Key == ConsoleKey.Escape)
+            {
+                stopCalculator();
+                Core.Running = true;
+            }
+            else if (key.Key == ConsoleKey.Enter)
+            {
+                calculate();
+            }
+            else if (key.Key == ConsoleKey.Delete)
+            {
+                displayText = "";
+            }
+            else
+            {
+                var valid = false;
+                for (var i = 0; i < validChars.Length; i++)
+                {
+                    if (key.KeyChar == validChars[i])
+                    {
+                        valid = true;
+                    }
+                }
+                if (valid == true)
+                {
+                    if (displayText.Length < 16)
+                    {
+                        displayText += key.KeyChar;
+                    }
+                }
+                userInterface.WriteString(32, 8, displayText, ConsoleColor.White, ConsoleColor.Blue);
+            }
+            if (Core.Running == false)
+            {
+                checkKey();
+            }
         }
 
         public static void stopCalculator()
@@ -64,10 +105,33 @@ namespace TWIx86.Programs
             Console.Clear();
         }
 
-        public static void setToolbarStatus(string text,ConsoleColor bg, ConsoleColor fg)
+        public static void setToolbarStatus(string text, ConsoleColor bg, ConsoleColor fg)
         {
             userInterface.drawRectangle(0, 0, 80, 1, bg);
             userInterface.WriteString(0, 0, text, fg, bg);
         }
+
+        public static void start()
+        {
+            Core.Running = false;
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Clear();
+            Draw();
+            setToolbarStatus("TWIx86 Calculator", ConsoleColor.Cyan, ConsoleColor.White);
+            Console.SetCursorPosition(0, 0);
+
+            checkKey();
+        }
+
+        public static void calculate()
+        {
+            Draw();
+            var evaluator = new ExpressionEvaluator();
+            var x = evaluator.evaluate(evaluator.parse(displayText));
+            userInterface.WriteString(32, 8, x.ToString(), ConsoleColor.White, ConsoleColor.Blue);
+            displayText = "";
+        }
     }
 }
+
